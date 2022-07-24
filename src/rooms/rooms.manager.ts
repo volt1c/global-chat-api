944396e376651cc292e05a0bc8605ea67fa2255b
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-07-24 05:38:53 +0200

    refactor(rooms.manager): `roomsRepository` -> `repository`

--- src/rooms/rooms.manager.ts
+++ src/rooms/rooms.manager.ts
@@ -7,26 +7,26 @@ import { RoomsRepository } from './rooms.repository'
 @Injectable()
 export class RoomsManager {
   constructor(
-    private roomsRepository: RoomsRepository,
+    private repository: RoomsRepository,
     private emiter: RoomsEmiter,
   ) {}
 
   join(client: Socket, roomId: string) {
-    if (!this.roomsRepository.findOne(roomId))
+    if (!this.repository.findOne(roomId))
       throw new WsException('incorrect room id')
 
-    if (this.roomsRepository.findByClientId(client.id)) {
+    if (this.repository.findByClientId(client.id)) {
       this.emiter.emitLeave(client)
-      this.roomsRepository.deleteClient(client.id)
+      this.repository.deleteClient(client.id)
     }
 
-    this.roomsRepository.setClient(roomId, client)
+    this.repository.setClient(roomId, client)
     this.emiter.emitJoin(client)
   }
 
   leave(client: Socket) {
     this.emiter.emitLeave(client)
-    this.roomsRepository.deleteClient(client.id)
+    this.repository.deleteClient(client.id)
   }
 
   message(client: Socket, message: string) {
